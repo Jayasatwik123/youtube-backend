cd8569bbff722c3a50c28ba06efeb9fd29807e74
Jayasatwik123 <132732183+Jayasatwik123@users.noreply.github.com>
2023-06-19 15:52:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import videoRoutes from "./routes/videos.js"
 import commentRoutes from "./routes/comments.js"
 import authRoutes from "./routes/auth.js"
 import cookieParser from "cookie-parser";
-
+const cors=require("cors");
 const app=express()
 const connect=()=>{
     mongoose.connect("mongodb+srv://jaya:jayasatwik1234@cluster0.f2iyvoz.mongodb.net/youtube?retryWrites=true&w=majority")
@@ -15,6 +15,7 @@ const connect=()=>{
       throw err;
     })
 }
+app.use(cors());
 app.use(cookieParser())
 
 app.use(express.json())
